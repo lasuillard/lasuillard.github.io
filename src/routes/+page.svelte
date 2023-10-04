a9d3f17a19ba8f60ad38ee810477f0c17f02e773
Yuchan Lee <lasuillard@gmail.com>
2023-10-04 11:02:17 +0000

    build(deps-dev): Setup Tailwind CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="m-2 p-1">
+	<h1 class="text-3xl font-extrabold mb-2">Welcome to SvelteKit</h1>
+	<p class="text-xl">
+		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
