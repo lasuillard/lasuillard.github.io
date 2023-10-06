c5e2fcc4a33d762a2d56d292f5d1a3dd4529f683
Yuchan Lee <lasuillard@gmail.com>
2023-10-06 00:30:24 +0000

    test: Refactor store test
    Generate random key for persistent store to avoid interference between
    store tests.

--- tests/lib/store.test.ts
+++ tests/lib/store.test.ts
@@ -5,8 +5,8 @@ import { describe, expect, it } from 'vitest';
 
 describe('persisted store', () => {
 	it('should conform to store interface and use browser local storage by default', () => {
-		const store = persisted('test', 'default-value');
-
+		const key = Math.random().toString();
+		const store = persisted(key, 'default-value');
 		expect(get(store)).toEqual('default-value');
 
 		let changed = '';
@@ -16,18 +16,18 @@ describe('persisted store', () => {
 
 		store.set('hello world');
 		expect(changed).toEqual('hello world');
-		expect(localStorage.getItem('test')).toEqual('"hello world"');
+		expect(localStorage.getItem(key)).toEqual('"hello world"');
 
 		store.update((value) => value + ', done');
 		expect(changed).toEqual('hello world, done');
-		expect(localStorage.getItem('test')).toEqual('"hello world, done"');
+		expect(localStorage.getItem(key)).toEqual('"hello world, done"');
 	});
 
 	it('should compatible with browser session storage', () => {
-		const store = persisted('test', 'default-value', {
+		const key = Math.random().toString();
+		const store = persisted(key, 'default-value', {
 			storage: sessionStorage
 		});
-
 		expect(get(store)).toEqual('default-value');
 
 		let changed = '';
@@ -37,10 +37,10 @@ describe('persisted store', () => {
 
 		store.set('hello world');
 		expect(changed).toEqual('hello world');
-		expect(sessionStorage.getItem('test')).toEqual('"hello world"');
+		expect(sessionStorage.getItem(key)).toEqual('"hello world"');
 
 		store.update((value) => value + ', done');
 		expect(changed).toEqual('hello world, done');
-		expect(sessionStorage.getItem('test')).toEqual('"hello world, done"');
+		expect(sessionStorage.getItem(key)).toEqual('"hello world, done"');
 	});
 });
