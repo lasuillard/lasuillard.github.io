0bafd7f33b256c0fdfe9449d497cedbf8dcd542c
Yuchan Lee <lasuillard@gmail.com>
2023-10-09 07:25:44 +0000

    test: Test error page

--- tests/routes/error.test.ts
+++ tests/routes/error.test.ts
@@ -1,3 +1,23 @@
-import { it } from 'vitest';
+// @vitest-environment jsdom
+import Page from '$lib/../routes/+error.svelte';
+import { render } from '@testing-library/svelte';
+import { describe, expect, it } from 'vitest';
 
-it.todo('error');
+describe('error', () => {
+	it('shows error detail', () => {
+		const { getByText } = render(Page, {
+			status: 404,
+			message: 'Not Found'
+		});
+		expect(getByText('404')).toBeTruthy();
+		expect(getByText('Not Found')).toBeTruthy();
+	});
+
+	it('provides alternative message if not given', () => {
+		const { getByText } = render(Page, {
+			status: 404
+		});
+		expect(getByText('404')).toBeTruthy();
+		expect(getByText('Unknown Error')).toBeTruthy();
+	});
+});
