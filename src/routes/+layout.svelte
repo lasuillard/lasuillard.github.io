26b71d6f2de8cf958db93ba8eb2be563b5d04474
Yuchan Lee <lasuillard@gmail.com>
2023-10-16 06:59:00 +0000

    fix: Layout breaks
    Fixes #19.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,12 +10,15 @@
 <div data-testid="layout" class="grid min-h-screen auto-rows-min grid-cols-1">
 	<Header class="sticky top-0 z-[1] bg-base-200" bind:openSidebar />
 
-	<div class="grid grid-flow-row p-3 md:grid-flow-col">
+	<div class="grid grid-cols-1 p-3 md:grid-cols-4 xl:grid-cols-6">
 		<!-- NOTE: Bind to internal checkbox to apply CSS with Tailwind CSS easily -->
 		<input type="checkbox" bind:checked={openSidebar} class="peer hidden" />
 		<Sidebar class="col-span-1 p-3 peer-checked:hidden" />
 
-		<main class="px-3 py-6 md:col-span-3 lg:px-12 lg:py-24 xl:col-span-5" data-testid="main">
+		<main
+			class="peer-checked:xl-col-span-6 col-span-3 px-3 py-6 peer-checked:md:col-span-4 lg:px-12 lg:py-24 xl:col-span-5"
+			data-testid="main"
+		>
 			<!-- TODO: Breadcrumbs -->
 			<slot />
 		</main>
