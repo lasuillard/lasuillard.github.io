e40ef29fc854b75033889236fb0e136b91f53533
Yuchan Lee <lasuillard@gmail.com>
2023-10-17 08:54:49 +0000

    feat: Highlight code blocks with **highlight.js**

--- src/components/Markdown.svelte
+++ src/components/Markdown.svelte
@@ -1,3 +1,16 @@
+<script lang="ts">
+	import 'highlight.js/styles/atom-one-dark-reasonable.css';
+
+	// Frontmatter passed as props
+	// NOTE: Variables not specified here will emit warning in console "<Markdown> was created with unknown prop 'title'".
+	export let title;
+	export let publicationDate;
+	export let tags;
+
+	// Meaningless code to suppress linter warnings
+	[title, publicationDate, tags];
+</script>
+
 <article class="prose max-w-none lg:prose-lg" data-testid="markdown">
 	<slot />
 </article>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import hljs from 'highlight.js';
 import { mdsvex } from 'mdsvex';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
@@ -12,7 +13,15 @@ const mdsvexConfig = {
 	layout: {
 		_: 'src/components/Markdown.svelte' // NOTE: You can't use alias here
 	},
-	rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]]
+	rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
+	highlight: {
+		// https://github.com/pngwn/MDsveX/issues/514
+		highlighter(code, lang) {
+			const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+			const html = hljs.highlight(code, { language }).value;
+			return `<pre class="language-${lang}">{@html \`<code class="language-${lang}">${html}</code>\`}</pre>`;
+		}
+	}
 };
 
 // https://kit.svelte.dev/docs/configuration
