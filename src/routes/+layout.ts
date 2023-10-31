7df0ee0a23a8be4e22edbaf91364e81ebdf7399a
Yuchan Lee <lasuillard@gmail.com>
2023-10-31 08:01:57 +0000

    revert: "build: Set `trailingSlash` to `"always"`"
    This reverts commit 453c1dd69a8c6b71ef4c551e386298e77794d3d3.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,7 +2,6 @@ import type { LayoutLoad } from './$types';
 
 export const prerender = true;
 export const ssr = true;
-export const trailingSlash = 'always';
 
 export const load: LayoutLoad = ({ url }) => {
 	return {
