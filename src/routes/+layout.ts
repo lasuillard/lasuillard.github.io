453c1dd69a8c6b71ef4c551e386298e77794d3d3
Yuchan Lee <lasuillard@gmail.com>
2023-10-31 07:54:12 +0000

    build: Set `trailingSlash` to `"always"`

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,7 @@ import type { LayoutLoad } from './$types';
 
 export const prerender = true;
 export const ssr = true;
+export const trailingSlash = 'always';
 
 export const load: LayoutLoad = ({ url }) => {
 	return {
