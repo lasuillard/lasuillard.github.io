dadb142cb82866ea3896c803ba2db3b21eb2ccc9
Yuchan Lee <lasuillard@gmail.com>
2023-12-27 13:08:32 +0000

    build(deps-dev): Bump deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 // https://kit.svelte.dev/docs/configuration
 // eslint-disable-next-line jsdoc/check-tag-names
