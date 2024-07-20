db863e5a338371d28dde9bbefcf3286954092cd0
Yuchan Lee <lasuillard@gmail.com>
2024-07-20 02:02:26 +0000

    feat: Update about me

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -97,8 +97,6 @@ Python 외에도 TypeScript, Rust에도 관심이 많아 토이 프로젝트를 
 						end: new Date('2021-12-31')
 					},
 					description: `
-2명의 백엔드 개발자 및 1명의 프론트엔드 개발자와 함께 서비스 개발 및 출시
-
 - Django 기반 선정산 서버 개발
   - 데이터베이스 설계 및 ORM 구현
   - Django Admin을 이용한 정산 관리자 페이지 개발
@@ -149,8 +147,6 @@ Python 외에도 TypeScript, Rust에도 관심이 많아 토이 프로젝트를 
 						end: new Date('2023-08-31')
 					},
 					description: `
-1 BE, 1 FE, 2 ML 동료 개발자와 함께 ABACUS AutoML 솔루션의 개발 및 유지보수
-
 - 레거시 애플리케이션 유지보수
 - 고객사 (우리FIS) 장애 대응
 - 차세대 AutoMLOps 솔루션 ABACUS Enterprise 설계 및 기획 참여
@@ -173,8 +169,6 @@ Python 외에도 TypeScript, Rust에도 관심이 많아 토이 프로젝트를 
 						end: new Date('2023-08-31')
 					},
 					description: `
-2명의 동료 백엔드 개발자와 함께 우리카드 FDS의 레거시 딥러닝 스코어링 서버 유지보수 및 신규 기능 개발, 성능 개선
-
 - 레거시 우리카드 FDS 시스템의 유지보수 및 신규 기능 개발
   - 일일 약 700만건의 요청을 처리하는 Python Twisted 기반 비동기 TCP 서버 애플리케이션 유지보수
   - 딥러닝 스코어링 요청 처리 다중 프로세스 Python 애플리케이션 유지보수
@@ -196,41 +190,72 @@ Python 외에도 TypeScript, Rust에도 관심이 많아 토이 프로젝트를 
 					)
 				}
 			]
+		},
+		{
+			organization: '얼리페이',
+			role: '백엔드 개발자',
+			summary: '재직 중',
+			period: {
+				start: new Date('2023-12-04'),
+				end: new Date(Date.now())
+			},
+			projects: []
 		}
 	];
 
 	const personalWorks = [
 		{
 			name: 'lasuillard.github.io',
-			description: `바닥부터 직접 개발한 기술 블로그 정적 웹 사이트`,
+			description: `SvelteKit을 이용하여 직접 처음부터 만든 개인 블로그입니다.`,
 			link: 'https://github.com/lasuillard/lasuillard.github.io',
 			tags: tags('GitHub Actions', 'SvelteKit', 'Tailwind CSS'),
 			status: 'WIP',
 			order: 0 // Ascending order
 		},
 		{
-			name: 'kkowa',
-			description: `중간자 프록시를 이용하여 타사 애플리케이션의 네트워크 데이터를 통해 유용한 정보를 추출, 가공하기 위한 GUI 애플리케이션`,
-			link: 'https://github.com/lasuillard/kkowa',
-			tags: tags('GitHub Actions', 'gRPC', 'mitmproxy', 'PySide6 (Qt) ', 'Python', 'SQLAlchemy'),
-			status: 'WIP',
+			name: 'raindrop-client',
+			description: `
+Raindrop.io API의 비공식 OpenAPI 스키마 정의 및 자동 생성된 클라이언트 라이브러리입니다.
+
+Raindrop.io에서 제공하지 않는 OpenAPI 스키마를 직접 정의하고 OpenAPI Generator를 이용하여 API 클라이언트 코드를 자동 생성합니다.
+[npm](https://www.npmjs.com/package/@lasuillard/raindrop-client)에 배포되어 있습니다.
+        `,
+			link: 'https://github.com/lasuillard/raindrop-client',
+			tags: tags('TypeScript', 'GitHub Actions', 'OpenAPI', 'OpenAPI Generator'),
+			status: 'Published',
 			order: 1
 		},
 		{
-			name: 'Libra',
-			description: `PC의 수많은 파일을 탐색 및 라벨링하여 체계적으로 관리하기 위한 GUI 애플리케이션`,
-			link: 'https://github.com/lasuillard/Libra',
-			tags: tags('GitHub Actions', 'Rust', 'SvelteKit', 'Tailwind CSS', 'Tauri', 'TypeScript'),
+			name: 'Raindrop Sync for Chrome',
+			description:
+				'Raindrop.io와 크롬 브라우저간 북마크 동기화 기능을 제공하기 위한 크롬 브라우저 확장 프로그램입니다.',
+			link: 'https://github.com/lasuillard/raindrop-sync-chrome',
+			tags: tags('Chrome Extension', 'GitHub Actions', 'Svelte', 'Tailwind CSS', 'TypeScript'),
 			status: 'WIP',
 			order: 2
 		},
 		{
-			name: 'Raindrop Sync for Chrome',
-			description: `Raindrop.io와 크롬 브라우저간 북마크 동기화 기능을 제공하는 크롬 브라우저 확장 프로그램`,
-			link: 'https://github.com/lasuillard/raindrop-sync-chrome',
-			tags: tags('Chrome Extension', 'GitHub Actions', 'Svelte', 'Tailwind CSS', 'TypeScript'),
-			status: 'WIP',
-			order: 3
+			name: 'django-slack-tools',
+			description: `
+Django 프로젝트 내 Slack 메시징 및 봇 개발 편의성을 제공하기 위한 라이브러리입니다.
+[PyPI](https://pypi.org/project/django-slack-tools/)에 공개되어 있습니다.
+        `,
+			link: 'https://github.com/lasuillard/django-slack-tools',
+			tags: tags('Python', 'Django', 'Slack', 'GitHub Actions'),
+			status: 'Published',
+			order: 4
+		},
+		{
+			name: 'mockoon-novnc',
+			description: `
+Mockoon의 GUI를 웹 브라우저에서 실행할 수 있도록 하는 NoVNC 연동 및 부가 기능을 제공하기 위한 Docker 이미지입니다.
+
+개발 중 모의 서버 구성 및 관리 편의를 위해 개발되었으며, [Docker Hub](https://hub.docker.com/r/lasuillard/mockoon-novnc)에 공개되어 있습니다.
+`,
+			link: 'https://github.com/lasuillard/mockoon-novnc',
+			tags: tags('Docker', 'Mockoon', 'GitHub Actions'),
+			status: 'Published',
+			order: 5
 		}
 	];
 </script>
