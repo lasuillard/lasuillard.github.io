e5c6f8ae557fe7b530093f9d12a1282241303307
Yuchan Lee <lasuillard@gmail.com>
2024-09-21 22:21:32 +0900

    chore(devcontainer): Update `onCreateCommand.sh`
    Signed-off-by: Yuchan Lee <lasuillard@gmail.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -4,9 +4,3 @@
 curl -sL https://sentry.io/get-cli/ | sh
 
 sudo apt-get update && sudo apt-get install -y bash-completion
-
-echo '
-if [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-fi
-' >> ~/.bashrc
