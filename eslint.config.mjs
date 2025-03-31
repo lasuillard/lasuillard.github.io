87408321c080f8ab296f4c4241d020d0c30e3760
Yuchan Lee <lasuillard@gmail.com>
2025-03-31 11:45:38 +0000

    chore(deps-dev): Migrate ESLint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier/flat';
+import jsdoc from 'eslint-plugin-jsdoc';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
+
+export default ts.config(
+	{
+		ignores: [
+			'coverage/*',
+			'playwright-report/*',
+			'test-results/*',
+			'build/*',
+			'.svelte-kit/*',
+			'vite.config.{js,ts}.timestamp-*'
+		]
+	},
+	js.configs.recommended,
+	...ts.configs.recommended,
+	prettier,
+	...svelte.configs.recommended,
+	jsdoc.configs['flat/recommended-typescript'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig
+			}
+		}
+	},
+	{
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off'
+		}
+	}
+);
