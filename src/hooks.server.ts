10b1d33777ba8a3ae4f96a26c295a5e91406bcce
Yuchan Lee <lasuillard@gmail.com>
2025-06-17 15:11:30 +0000

    feat: Add config key to disable Sentry for server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,6 +4,7 @@ import { sequence } from '@sveltejs/kit/hooks';
 
 const currentEnv = env.ENVIRONMENT || 'unknown';
 const sentryDsn = env.SENTRY_DSN || '';
+const disableSentry = (env.DISABLE_SENTRY || '').length > 0;
 
 console.info('Current environment is:', currentEnv);
 if (sentryDsn) {
@@ -13,16 +14,20 @@ if (sentryDsn) {
 }
 
 export const handle = sequence(
-	Sentry.initCloudflareSentryHandle({
-		dsn: sentryDsn,
-		tracesSampleRate: 0.05,
-		environment: currentEnv,
-		integrations: [],
-		sendDefaultPii: true,
-		_experiments: {
-			enableLogs: true
-		}
-	}),
+	...(disableSentry
+		? [
+				Sentry.initCloudflareSentryHandle({
+					dsn: sentryDsn,
+					tracesSampleRate: 0.05,
+					environment: currentEnv,
+					integrations: [],
+					sendDefaultPii: true,
+					_experiments: {
+						enableLogs: true
+					}
+				})
+			]
+		: []),
 	Sentry.sentryHandle()
 );
 
