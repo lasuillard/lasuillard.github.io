9d762bfe8ac02974a79bbbde24011b27b295b181
Yuchan Lee <lasuillard@gmail.com>
2025-06-25 14:35:37 +0000

    fix: Search results dropdown not showing on focus

--- src/components/utility/Search.svelte
+++ src/components/utility/Search.svelte
@@ -3,18 +3,16 @@
 	import { getEngine } from '$lib/search';
 	import { quoteJoin } from '$lib/utils';
 	import type { SearchResult, Suggestion } from 'minisearch';
-	import { createBubbler, preventDefault } from 'svelte/legacy';
 	import { writable } from 'svelte/store';
 
-	const bubble = createBubbler();
 	const searchEngine = getEngine();
 
 	let searchText = writable('');
 	let searchResults: SearchResult[] = $state([]);
 	let suggestions: Suggestion[] = $state([]);
 
-	searchText.subscribe((text) => {
-		if (!text) {
+	searchText.subscribe((value) => {
+		if (!value) {
 			searchResults = [];
 			return;
 		}
@@ -24,7 +22,7 @@
 			return;
 		}
 
-		searchResults = searchEngine.search(text, { fuzzy: 0.15 });
+		searchResults = searchEngine.search(value, { fuzzy: 0.15 });
 		searchResults.sort(
 			// FIXME: Get results type annotated
 			(a, b) =>
@@ -32,11 +30,11 @@
 				a['metadata.title'].localeCompare(b['metadata.title'])
 		);
 		console.debug(
-			`Searching for "${text}": ${quoteJoin(searchResults.map((result) => result.id))}`
+			`Searching for "${value}": ${quoteJoin(searchResults.map((result) => result.id))}`
 		);
 
 		if (!searchResults.length) {
-			suggestions = searchEngine.autoSuggest(text, { fuzzy: 0.25 });
+			suggestions = searchEngine.autoSuggest(value, { fuzzy: 0.25 });
 			console.debug(
 				`No search result, making suggestion: ${quoteJoin(
 					suggestions.map((value) => value.suggestion)
@@ -48,12 +46,7 @@
 
 <div data-testid="search" class="mb-2 w-64">
 	<div class="group relative flex items-center space-x-2">
-		<form
-			class="w-full"
-			autocomplete="off"
-			onsubmit={preventDefault(bubble('submit'))}
-			role="search"
-		>
+		<div class="w-full" role="search">
 			<div class="absolute mt-[9px] ml-[11px]">
 				<Search class="h-4 w-4 stroke-gray-400" />
 			</div>
@@ -63,11 +56,15 @@
 				type="text"
 				placeholder="Search"
 				bind:value={$searchText}
-				class="peer input input-bordered h-8 w-full pl-8 placeholder:font-light"
+				class="input input-bordered h-8 w-full pl-8 placeholder:font-light"
 			/>
-			<div class="dropdown peer-[:focus]:dropdown-open absolute top-[135%] right-0 z-1 w-full">
-				{#if searchResults.length > 0}
-					<div role="searchbox" tabindex="0">
+			<div
+				class="dropdown absolute top-[135%] right-0 z-1 w-full {$searchText.length > 0
+					? 'dropdown-open'
+					: ''}"
+			>
+				{#if $searchText}
+					<div role="searchbox">
 						<ol
 							class="menu dropdown-content bg-base-200 w-full space-y-2 rounded-xs shadow-xl hover:visible!"
 						>
@@ -95,6 +92,6 @@
 					</div>
 				{/if}
 			</div>
-		</form>
+		</div>
 	</div>
 </div>
