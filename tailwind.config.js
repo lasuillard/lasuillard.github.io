9181541491aaa5e91c66a6fbfd63773b483b6ea3
Yuchan Lee <lasuillard@gmail.com>
2026-07-23 06:02:37 +0000

    Fix build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,5 @@
 export default {
 	// eslint-disable-next-line @typescript-eslint/no-require-imports
 	plugins: [require('@tailwindcss/typography')],
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			screens: {
-				'h-sm': {
-					raw: '(min-height: 480px)'
-				},
-				'h-md': {
-					raw: '(min-height: 640px)'
-				},
-				'h-lg': {
-					raw: '(min-height: 768px)'
-				}
-			}
-		}
-	}
+	content: ['./src/**/*.{html,js,svelte,ts}']
 };
